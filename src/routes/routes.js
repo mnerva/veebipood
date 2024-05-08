639ec8d9995ade46104a368fdec33651eba1f31c
mona <mona-rea.nerva@voco.ee>
2024-05-08 13:14:46 +0300

    Created links and buttons

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,15 +8,15 @@ const path = require('path');
 //});
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    res.sendFile(path.join(__dirname, '../../public', 'index.html'));
 });
 
 router.get('/checkout', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'checkout.html'));
+    res.sendFile(path.join(__dirname, '../../public', 'checkout.html'));
 });
 
 router.get('/confirmation', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'confirmation.html'));
+    res.sendFile(path.join(__dirname, '../../public', 'confirmation.html'));
 });
 
 module.exports = router;
